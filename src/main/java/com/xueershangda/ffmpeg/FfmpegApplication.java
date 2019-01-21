948d586fd8f68321f8cff8fd23f48e0e85eee652
skmbw <tongku2008@126.com>
2019-01-21 17:07:34 +0800

    init

---
+++ src/main/java/com/xueershangda/ffmpeg/FfmpegApplication.java
@@ -0,0 +1,14 @@
+package com.xueershangda.ffmpeg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FfmpegApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FfmpegApplication.class, args);
+	}
+
+}
+
---
+++ src/main/java/com/xueershangda/ffmpeg/LearnFFmpeg.java
@@ -0,0 +1,121 @@
+package com.xueershangda.ffmpeg;
+
+import net.bramp.ffmpeg.FFmpeg;
+import net.bramp.ffmpeg.FFmpegExecutor;
+import net.bramp.ffmpeg.FFmpegUtils;
+import net.bramp.ffmpeg.FFprobe;
+import net.bramp.ffmpeg.builder.FFmpegBuilder;
+import net.bramp.ffmpeg.job.FFmpegJob;
+import net.bramp.ffmpeg.probe.FFmpegFormat;
+import net.bramp.ffmpeg.probe.FFmpegProbeResult;
+import net.bramp.ffmpeg.probe.FFmpegStream;
+import net.bramp.ffmpeg.progress.Progress;
+import net.bramp.ffmpeg.progress.ProgressListener;
+
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * 对FFmpeg命令的封装学习
+ *
+ * @author yinlei
+ * @since 2019-1-21 15:13
+ */
+public class LearnFFmpeg {
+    public static final String PREFIX = "D:\\downloads\\ffmpeg-20190116-win64-static\\bin\\";
+
+    // Get Media Information
+    public static void main(String[] args) throws IOException {
+
+        FFprobe ffprobe = new FFprobe(PREFIX + "ffprobe.exe");
+        FFmpegProbeResult probeResult = ffprobe.probe(PREFIX + "input.mp4");
+
+        FFmpegFormat format = probeResult.getFormat();
+        System.out.format("%nFile: '%s' ; Format: '%s' ; Duration: %.3fs",
+                format.filename,
+                format.format_long_name,
+                format.duration
+        );
+
+        FFmpegStream stream = probeResult.getStreams().get(0);
+        System.out.format("%nCodec: '%s' ; Width: %dpx ; Height: %dpx",
+                stream.codec_long_name,
+                stream.width,
+                stream.height
+        );
+    }
+
+    // 视频编码
+    public static void videoEncoding() throws IOException {
+        FFmpeg ffmpeg = new FFmpeg(PREFIX + "ffmpeg.exe");
+        FFprobe ffprobe = new FFprobe(PREFIX + "ffprobe.exe");
+
+        FFmpegBuilder builder = new FFmpegBuilder()
+
+                .setInput("input.mp4")     // Filename, or a FFmpegProbeResult
+                .overrideOutputFiles(true) // Override the output if it exists
+
+                .addOutput("output.mp4")   // Filename for the destination
+                .setFormat("mp4")        // Format is inferred from filename, or can be set
+                .setTargetSize(250_000)  // Aim for a 250KB file
+
+                .disableSubtitle()       // No subtiles
+
+                .setAudioChannels(1)         // Mono audio
+                .setAudioCodec("aac")        // using the aac codec
+                .setAudioSampleRate(48_000)  // at 48KHz
+                .setAudioBitRate(32768)      // at 32 kbit/s
+
+                .setVideoCodec("libx264")     // Video using x264
+                .setVideoFrameRate(24, 1)     // at 24 frames per second
+                .setVideoResolution(640, 480) // at 640x480 resolution
+
+                .setStrict(FFmpegBuilder.Strict.EXPERIMENTAL) // Allow FFmpeg to use experimental specs
+                .done();
+
+        FFmpegExecutor executor = new FFmpegExecutor(ffmpeg, ffprobe);
+
+        // Run a one-pass encode
+        executor.createJob(builder).run();
+
+        // Or run a two-pass encode (which is better quality at the cost of being slower)
+        executor.createTwoPassJob(builder).run();
+    }
+
+    public static void encodeWithListener() throws IOException {
+        FFmpeg ffmpeg = new FFmpeg(PREFIX + "ffmpeg.exe");
+        FFprobe ffprobe = new FFprobe(PREFIX + "ffprobe.exe");
+        FFmpegExecutor executor = new FFmpegExecutor(ffmpeg, ffprobe);
+
+        FFmpegProbeResult in = ffprobe.probe("input.flv");
+
+        FFmpegBuilder builder = new FFmpegBuilder()
+                .setInput(in) // Or filename
+                .addOutput("output.mp4")
+                .done();
+
+        FFmpegJob job = executor.createJob(builder, new ProgressListener() {
+
+            // Using the FFmpegProbeResult determine the duration of the input
+            final double duration_ns = in.getFormat().duration * TimeUnit.SECONDS.toNanos(1);
+
+            @Override
+            public void progress(Progress progress) {
+                double percentage = progress.out_time_ns / duration_ns;
+
+                // Print out interesting information about the progress
+                System.out.println(String.format(
+                        "[%.0f%%] status:%s frame:%d time:%s ms fps:%.0f speed:%.2fx",
+                        percentage * 100,
+                        progress.status,
+                        progress.frame,
+                        FFmpegUtils.toTimecode(progress.out_time_ns, TimeUnit.NANOSECONDS),
+                        progress.fps.doubleValue(),
+                        progress.speed
+                ));
+            }
+        });
+
+        job.run();
+    }
+}
